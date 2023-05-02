3d1eb1e3076c87ebc120b675af705de3cabf51f6
eurbanovskiy <evgeniyurbanovskiy@gmail.com>
2023-05-02 13:02:18 +0200

    Initial Commit

---
+++ KLM Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KLM Assignment
+//
+//  Created by Евгений Урбановский on 5/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
