6d1bdcf7b901422c744f17519ac87277df06922c
eurbanovskiy <evgeniyurbanovskiy@gmail.com>
2023-05-02 13:06:59 +0200

    Init commit

---
+++ KLM Assignment/Sources/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  KLM Assignment
+//
+//  Created by Евгений Урбановский on 5/2/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
